280b3528c7274f54da5c1d3896952007aae541d5
estraining <dylatos2022@gmail.com>
2020-06-27 00:58:29 +0800

    implementation of vga_buffers

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+#![no_std]
+#![no_main]
+use core::panic::PanicInfo;
+
+/// This function is called on panic.
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+  loop {}
 }
+
+mod vga_buffer;
+
+static HELLO: &[u8] = b"Hello World!";
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+  vga_buffer::print_something();
+  loop {}
+}
+
