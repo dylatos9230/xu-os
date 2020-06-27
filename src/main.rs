7f1f58574453917dc5308c541d2e24e398a3d9d4
estraining <dylatos2022@gmail.com>
2020-06-27 22:05:09 +0800

    integrate qemu

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,64 @@
 #![no_std]
 #![no_main]
+// Replacing the default test framework
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
+#![reexport_test_harness_main = "test_main"]
+
 use core::panic::PanicInfo;
+
+mod vga_buffer;
+mod serial;
 
 /// This function is called on panic.
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+  println!("{}", info);
   loop {}
 }
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(u32)]
+pub enum QemuExitCode {
+  Success = 0x10,
+  Failed = 0x11,
+}
+pub fn exit_qemu(exit_code: QemuExitCode){
+  use x86_64::instructions::port::Port;
 
-mod vga_buffer;
+  unsafe {
+    let mut port = Port::new(0xf4);
+    port.write(exit_code as u32);
+  }
+}
+#[cfg(test)]
+fn test_runner(tests: &[&dyn Fn()]) {
+    println!("Running {} tests", tests.len());
+    for test in tests {
+        test();
+    }
+    exit_qemu(QemuExitCode::Success);
+}
+
+#[test_case]
+fn trivial_assertion(){
+  print!("trival assertion...");
+  assert_eq!(1,1);
+  println!("[ok]");
+}
+
 
 static HELLO: &[u8] = b"Hello World!";
+
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-  vga_buffer::print_something();
+  println!("Hello World{}", "!");
+
+  #[cfg(test)]
+  test_main();
   loop {}
 }
+
+
+
+
 
--- src/vga_buffer.rs
+++ src/vga_buffer.rs
@@ -52,6 +52,17 @@ pub struct Writer {
   buffer: &'static mut Buffer,
 }
 
+use lazy_static::lazy_static;
+use spin::Mutex;
+lazy_static!{
+  pub static ref WRITER: Mutex<Writer> =Mutex::new(Writer{
+    column_position: 0,
+    color_code: ColorCode::new(Color::Yellow, Color::Black),
+    buffer: unsafe{ &mut *(0xb8000 as *mut Buffer)}
+  });
+}
+
+
 impl Writer {
   pub fn write_byte(&mut self, byte: u8){
     match byte {
@@ -72,7 +83,7 @@ impl Writer {
       }
     }
   }
-  
+
   fn new_line(&mut self) {
     for row in 1..BUFFER_HEIGHT{
       for col in 0..BUFFER_WIDTH{
@@ -114,6 +125,7 @@ impl fmt::Write for Writer {
   }
 }
 
+
 pub fn print_something(){
   use core::fmt::Write;
   let mut writer = Writer{
@@ -128,6 +140,21 @@ pub fn print_something(){
   write!(writer," The numbers are {} and {}", 42, 1.0/3.0).unwrap();
 }
 
+#[macro_export]
+macro_rules! print {
+    ($($arg:tt)*) => ($crate::vga_buffer::_print(format_args!($($arg)*)));
+}
 
+#[macro_export]
+macro_rules! println {
+    () => ($crate::print!("\n"));
+    ($($arg:tt)*) => ($crate::print!("{}\n", format_args!($($arg)*)));
+}
+
+#[doc(hidden)]
+pub fn _print(args: fmt::Arguments) {
+    use core::fmt::Write;
+    WRITER.lock().write_fmt(args).unwrap();
+}
 
 
